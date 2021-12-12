0ac9673152960f27188823c780e92f3578ccabd8
Simplehacker1Community <93964893+Simplehacker1Community@users.noreply.github.com>
2021-12-12 15:47:33 +0530

    Add files via upload

---
+++ love.sh
@@ -0,0 +1,14 @@
+#Auther : Rajni
+apt install espeak python -y 
+rm loveyou2.txt
+rm loveyou.txt
+rm logo.py
+rm logo.py
+clear
+wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/logo.py
+wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/loveyou.txt
+wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/loveyou2.txt
+clear
+python logo.py
+espeak -v Annie -p8 -s150 -k28 -f loveyou2.txt
+espeak -v Annie -p8 -s150 -k28 -f loveyou.txt
