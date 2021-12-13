9c054c280625209ba5eeb0e3035d9afc84b72bb3
Simplehacker1Community <93964893+Simplehacker1Community@users.noreply.github.com>
2021-12-13 11:32:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,12 +11,9 @@ wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simple
 wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/loveyou2.txt
 wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/love.sh
 wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/simple.txt
-mkdir rajni
-cd rajni
 wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/rajni
 clear
 chmod +x * rajni
-mv rajni ../
 
 clear
 echo " installed love virus"
