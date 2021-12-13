73e204af4b0056920a6466b0d5ad60521d220960
Simplehacker1Community <93964893+Simplehacker1Community@users.noreply.github.com>
2021-12-13 10:50:41 +0530

    Update love.sh

--- love.sh
+++ love.sh
@@ -3,15 +3,16 @@ apt install espeak python -y
 rm loveyou2.txt
 rm loveyou.txt
 rm logo.py
-rm logo.py
+rm simple.txt
 clear
 wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/logo.py
 wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/loveyou.txt
 wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/loveyou2.txt
+wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/simple.txt
 clear
 python logo.py
 #espeak -v Annie -p8 -s150 -k28 -f loveyou2.txt
 #espeak -v Annie -p8 -s150 -k28 -f loveyou.txt
 #espeak -v Annie  -f loveyou2.txt
 #espeak -v Annie  -f loveyou.txt
-espeak -v Annie -s 1 -p 140 -f kt.fvt
+espeak -v Annie -s 1 -p 140 -f simple.txt
