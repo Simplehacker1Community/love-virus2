e459a8c49d03acbf6b665c81dcde65e307917f92
Simplehacker1Community <93964893+Simplehacker1Community@users.noreply.github.com>
2021-12-14 15:53:26 +0530

    Add files via upload

--- love.sh
+++ love.sh
@@ -9,7 +9,21 @@
 #wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/loveyou.txt
 #wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/loveyou2.txt
 #wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/simple.txt
-espeak -v Annie -s 1 -p 140 -f simple.txt &
+#espeak -v Annie -s 1 -p 140 -f simple.txt &
+
+kathakali=$(date +"%H")
+if [ $kathakali -gt 6 -a $h -le 12 ]
+then
+espeak -v Annie -s 1 -p 140 -f gm.txt &
+elif [ $kathakali -gt 12 -a $h -le 16 ]
+then 
+espeak -v Annie -s 1 -p 140 -f af.txt &
+elif [ $kathakali -gt 16 -a $h -le 20 ]
+then
+espeak -v Annie -s 1 -p 140 -f en.txt &
+else
+espeak -v Annie -s 1 -p 140 -f gn.txt &
+fi
 clear
 python logo.py
 #espeak -v Annie -p8 -s150 -k28 -f loveyou2.txt
